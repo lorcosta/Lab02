2c6a129385b4b23c54848470e253eb4761988505
lorenzocosta <lorenzocosta@mbp-di-lorenzo>
2020-03-18 21:18:28 +0100

    implementato doTranslate (con errori)

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -6,6 +6,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -20,11 +22,39 @@ public class FXMLController {
     
     @FXML
     private Button btnReset;
+  
+   @FXML
+   private TextField txtParole;
+   
+   @FXML
+   private TextArea txtRisultati;
+   
+   AlienDictionary ad;
 
     @FXML
     void doTranslate(ActionEvent event) {
+    	//Acquisisco il testo in una stringa
+    	String text= txtParole.getText();
+    	//Controllo il testo: è formato da una o da due parole? 
+    	String[] parole=text.split(" ");
+    	String alienWord= parole[0];
+    	String traduzione=parole[1];
+    	//Chiamo i metodi necessari
+    	if(parole.length==1) {
+    		//chiamo metodo di traduzione
+    		String parolaTradotta=ad.translateWord(alienWord);
+    	}else if(parole.length==2) {
+    		//chiamo metodo di aggiunta parola al dizionario
+    		ad.addWord(alienWord, traduzione);
+    	}else {
+    		//le parole non sono ne 2 e ne 1 perciò il formato inserito è sbagliato, lanciare un errore
+    		
+    	}
     	
-    	// TODO - add the button and complete this    	
+    	//Ripulisco casella di testo
+    	txtParole.clear();
+    	txtRisultati.setText("Aggiunta parola al dizionario");
+    	  	
     }
     
     
